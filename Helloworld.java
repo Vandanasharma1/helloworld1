baf2ff4067a5df3ca27ca845e026fe3d00a52c70
Vandanasharma1 <vandana.shar2207@gmail.com>
2025-11-16 13:12:47 +0530

    Change greeting message in Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 class Helloworld{  
     public static void main(String args[]){  
-     System.out.println("Hello Vandana");
+     System.out.println("Hello Good morning");
       System.out.println("This is Vandana sharma");
     }  
 }
